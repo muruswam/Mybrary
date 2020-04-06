456934c65001c99e5689b4bdbe6c0e79f511cf60
muruswam <swaminathan.murugan@gmail.com>
2020-04-06 20:37:12 +0530

    updarted crud for author module

--- models/author.js
+++ models/author.js
@@ -1,10 +1,26 @@
 const mongoose = require("mongoose")
+const Book = require('./book')
 
 const authorSchema = new mongoose.Schema({
     name: {
     type: String,
     required: true
     }
+})
+
+authorSchema.pre('remove', function(next) {
+    Book.find({author: this.id}, (error, books) =>{
+        if(error){
+            console.log(" error " + error)
+            next(error)
+        }else if(books.length > 0){
+            console.log(" inside books lenght "+"This author can't be deleted, as book still associated with him")
+            next(new Error("This author can't be deleted, as book still associated with him"))
+        }else {
+            console.log(" next()")
+            next()
+        }
+    })
 })
 
 module.exports =   mongoose.model("Author", authorSchema)
--- routes/author.js
+++ routes/author.js
@@ -1,15 +1,12 @@
 const express = require('express')
 const authorRouter = express.Router()
 const Author = require("../models/author")
+const Book = require('../models/book')
 
 
-authorRouter.get('/', async (req, res) => {
+authorRouter.get('/',  async (req, res) => {
     try {
-    let searchCriteria = {}
-    if(req.query.name != null && req.query.name != ''){
-        searchCriteria.name = new RegExp(req.query.name, 'i')
-    }
-    const authors = await Author.find(searchCriteria)
+    const authors =  await getAuthors(req);   
     res.render("author/index.ejs", { authors: authors})
 
     }
@@ -46,9 +43,14 @@ authorRouter.post('/',  async (req, res) => {
 })
 
 authorRouter.get('/:id', async (req, res) => {
+    try {
     const author = await Author.findById(req.params.id)
-
-    res.render("author/view" , {'id': req.params.id, 'name': author.name})
+    const books = await Book.find({author: author.id}).limit(2)
+    res.render("author/view" , {'id': req.params.id, 'name': author.name, 'books': books})
+    }catch(e){
+        console.log(e)
+        res.redirect("/")
+    }
 })
 
 authorRouter.get('/:id/edit', async (req, res) => {
@@ -75,6 +77,7 @@ authorRouter.put('/:id', async (req, res) => {
 
         let updatedAuthor = await author.save()
         res.redirect("/authors")
+
     }catch(err){
         res.render("author/edit", {"errorMessage": "Update failed !!!"+err, 'id': req.params.id, 'name': req.body.name})
     }
@@ -82,9 +85,41 @@ authorRouter.put('/:id', async (req, res) => {
     
 })
 
-authorRouter.delete('/:id', (req, res) => {
-    res.send("delete author"+req.params.id)
+authorRouter.delete('/:id', async (req, res) => {
+    let authors = await getAuthors(req)
+    try {
+        const author = await Author.findById(req.params.id)
+        if(author == null ) {
+            
+            res.render("author/index", {"errorMessage": "Delete Failed, Author Not Found", 'authors': authors})
+        }     
+
+        let updatedAuthor = await author.remove(/*(error, author) => {
+            if(error){
+                console.log('inside error callback')
+                
+                return res.render("author/index", {"errorMessage": "Delete Failed: "+error,  'authors': authors})
+            }
+        }*/)
+        let authors = await getAuthors(req)
+        console.log('inside edn of delete')
+        res.render("author/index",  {"errorMessage": "Deleted Successfully.",  'authors': authors})
+        
+    }catch(err){
+        
+        res.render("author/index", {"errorMessage": "Delete Failed: "+err, 'authors': authors})
+    }
+
 })
 
+ async function getAuthors(req){
+    let searchCriteria = {}
+    if(req.query.name != null && req.query.name != ''){
+        searchCriteria.name = new RegExp(req.query.name, 'i')
+    }
+    const authors =  await Author.find(searchCriteria)
+
+    return authors;
+}
 
 module.exports = authorRouter
